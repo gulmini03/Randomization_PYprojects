b6217a1a15fb9826f28f1a6acd4cbcf5850b06ca
gulmini03 <139476738+gulmini03@users.noreply.github.com>
2023-09-05 12:59:52 +0530

    Add files via upload

---
+++ Hangman Game.py
@@ -0,0 +1,42 @@
+import random
+import hangman_art
+import hangman_words
+
+wordf = random.choice(hangman_words.word_list)
+word_length = len(wordf)
+print(hangman_art.logo)
+
+display = []
+end = False
+chance = 6
+
+print(f"It is a {word_length} letter word.")
+
+for _ in range(word_length):
+    display += '_'
+    
+while end is False:
+    guess = input("Guess a letter: ")
+          
+    for j in range(word_length):
+        letter = wordf[j]
+        if letter == guess:
+            display[j] = letter
+
+    if guess not in wordf:
+        chance -= 1
+        print(f"'{guess}' is not in the word. {chance} chances left.")
+        if chance == 0:
+            end = True
+
+            print("You lose.")
+            print(f"The Word is '{wordf}'.")
+            
+    print(f"{' '.join(display)}")
+
+    if "_" not in display:
+        end is True
+        print("You win.")
+        exit()
+
+    print(hangman_art.stages[chance])
